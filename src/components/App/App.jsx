d5aaa109d6b3d8d6894a12d87d54a2b1f25d9761
AdelinaMakarevich <110402319+AdelinaMakarevich@users.noreply.github.com>
2023-11-01 00:29:53 +0300

    final

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
-import { getSearchId } from './store/actionCreators/ticketsCreators'
-import img from './components/assets/Logo.png'
-import TicketsList from './components/TicketsList'
-import Filter from './components/Filter'
+import { getSearchId } from '../../store/actionCreators/ticketsCreators'
+import img from '../assets/Logo.png'
+import TicketsList from '../TicketsList'
+import Filter from '../Filter'
+
 import styles from './App.module.scss'
 
 const App = () => {
